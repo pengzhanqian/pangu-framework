62ae9988630ac3f8d1f33c3458af5be725a6d18e
qianpengzhan <873373549@qq.com>
2022-05-18 14:21:38 +0800

    字符串工具类添加

---
+++ pangu-basic/pangu-basic-tools/src/main/java/com/pangu/framework/utils/PanguStringUtil.java
@@ -0,0 +1,15 @@
+package com.pangu.framework.utils;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * 继承自 hutool 的 StrUtil
+ * 字符串处理工具类
+ *
+ * @author pengzhan.qian
+ * @date 2022/5/18 14:15
+ * @see StrUtil
+ */
+public class PanguStringUtil extends StrUtil {
+
+}
